bd9bddeb96703bd1d24f91436f94d59755f41453
hibo hire <hibohire65@gmail.com>
2026-09-17 21:16:29 -0700

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-// Write your solution here!
+
+let cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+  cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+  cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+  cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+  cats.shift();
+}
+
+function destructivelyRemoveLastCat() {
+  cats.pop();
+}
+
+function destructivelyAppendCat(name) {
+  cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+  cats.unshift(name);
+}
+
+function removeFirstCat() {
+  return cats.slice(1);
+}
+
+function removeLastCat() {
+  return cats.slice(0, -1);
+}
+
+function appendCat(name) {
+  return [...cats, name];
+}
+
+function prependCat(name) {
+  return [name, ...cats];
+}
